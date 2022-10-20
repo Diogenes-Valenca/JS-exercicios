6052be20a05f90826abcfff9e3ac5ec0b1887264
Diogenes-Valenca <diovalenca@gmail.com>
2022-10-20 19:07:04 -0300

    Exercicio 1 feito

---
+++ exercicio1.js
@@ -0,0 +1,17 @@
+let a = 7
+let b = 6
+
+let soma = a + b
+console.log(soma)
+
+let subtracao = a - b
+console.log(subtracao)
+
+let multip = a * b
+console.log(multip)
+
+let divis = a / b
+console.log(divis)
+
+let modulo = a % b
+console.log(modulo)
